5bc97df30bc7b2d3c2c5ecd10b2a32076b990737
Tejas <137335372+Tru735@users.noreply.github.com>
2025-12-01 18:17:48 +0530

    flask api added

---
+++ week1/app.py
@@ -0,0 +1,27 @@
+import pickle
+from flask import Flask, request, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+## load
+model = pickle.load(open('boston_model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    model_pred = model.predict(new_data)
+    return jsonify({'prediction': model_pred[0]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
